13f2839f15e5f0f37cf255bd5bf9661a1ba02ab3
Michał <michalpeksa89@gmail.com>
2020-08-04 22:56:10 +0200

    Yo

---
+++ src/main/java/test/nr2/demo/HelloController.java
@@ -0,0 +1,14 @@
+package test.nr2.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
